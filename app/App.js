1ff011f0843d23a6dfbe2abc3ac4c7e656633d1b
Jonathan Liu <liu.z.jonathan@gmail.com>
2021-02-20 00:59:48 -0800

    taskboard complete

--- app/App.js
+++ app/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
-import Welcome from './src/screens/splash';
+import Welcome from './src/screens/welcome';
+import TaskBoard from './src/screens/task_board';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator>
-        <Stack.Screen 
+    <Stack.Navigator
+      initialRouteName="TaskBoard"
+    >
+      <Stack.Screen 
         name="Welcome" 
         component={Welcome} 
+        options={{ headerShown: false}} 
+      />
+      <Stack.Screen
+        name="TaskBoard"
+        component={TaskBoard}
         options={{ headerShown: false}} 
       />
        
